5dd4db294c7e1d71d95eaa4123aa9e1305f28b43
Rex Escario <rex_escario@yahoo.com>
2016-11-26 00:05:11 +0800

    feature: configured model associations.

--- server/sqldb/index.js
+++ server/sqldb/index.js
@@ -1,10 +1,5 @@
-/**
- * Sequelize initialization module
- */
-
 'use strict';
 
-import path from 'path';
 import config from '../config/environment';
 import Sequelize from 'sequelize';
 
@@ -13,8 +8,36 @@ var db = {
   sequelize: new Sequelize(config.sequelize.uri, config.sequelize.options)
 };
 
-// Insert models below
 db.Thing = db.sequelize.import('../api/thing/thing.model');
 db.User = db.sequelize.import('../api/user/user.model');
+
+db.Post = db.sequelize.import('../api/post/model');
+db.Post.belongsTo(db.User);
+db.Post.belongsTo(db.Post, { as: 'Parent' });
+db.User.hasMany(db.Post);
+db.Post.hasMany(db.Post, { as: 'PostReplies', foreignKey: 'ParentId' });
+
+db.PostLike = db.sequelize.import('../api/post-like/model');
+db.PostLike.belongsTo(db.Post);
+db.PostLike.belongsTo(db.User);
+db.Post.hasMany(db.PostLike);
+db.User.hasMany(db.PostLike);
+
+db.Notification = db.sequelize.import('../api/notification/model');
+db.Notification.belongsTo(db.User);
+db.PostLike.hasMany(db.Notification, {
+  foreignKey: 'NotifiableId',
+  constraints: false,
+  scope: {
+    Notifiable: 'PostLike'
+  }
+});
+db.Post.hasMany(db.Notification, {
+  foreignKey: 'NotifiableId',
+  constraints: false,
+  scope: {
+    Notifiable: 'Post'
+  }
+});
 
 module.exports = db;
