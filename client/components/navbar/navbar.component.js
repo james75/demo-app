48ec496f5911f6b1093678dc08d2f91e2ba208e5
Rex Escario <rex_escario@yahoo.com>
2016-11-26 00:06:58 +0800

    feature: can view new notifications count and reset once openend. can list notifications.

--- client/components/navbar/navbar.component.js
+++ client/components/navbar/navbar.component.js
@@ -11,15 +11,34 @@ export class NavbarComponent {
 
   isCollapsed = true;
 
-  constructor(Auth) {
-    'ngInject';
-
+  constructor(http, Auth) {
+    this.http = http;
     this.isLoggedIn = Auth.isLoggedInSync;
     this.isAdmin = Auth.isAdminSync;
     this.getCurrentUser = Auth.getCurrentUserSync;
   }
 
+  $onInit() {
+    this.http
+      .get('/api/notifications')
+      .then(response => {
+        const { meta, notifications } = response.data;
+        this.notifications = notifications;
+        this.meta = meta;
+      });
+  }
+
+  openNotifications() {
+    this.meta.new = 0;
+    this.http.post('/api/notifications/open');
+  }
+
 }
+
+NavbarComponent.$inject = [
+  '$http',
+  'Auth'
+];
 
 export default angular.module('directives.navbar', [])
   .component('navbar', {
