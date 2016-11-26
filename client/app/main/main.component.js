c87f2b36c7f497d54ac2a6c87a80c6a269bcee74
Rex Escario <rex_escario@yahoo.com>
2016-11-26 18:55:14 +0800

    refactor: used notifications-button and like-summary components.

--- client/app/main/main.component.js
+++ client/app/main/main.component.js
@@ -2,14 +2,18 @@
 
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
-import routing from './main.routes';
+import Route from './main.routes';
+import NotificationsButtonComponent from './components/notifications-button';
+import LikeSummaryComponent from './components/like-summary';
 
-export class Controller {
-  posts = [];
+import UserService from './services/user';
+import NotificationService from './services/notification';
+import PostService from './services/post';
 
-  constructor(http, auth) {
-    this.http = http;
+class Controller {
 
+  constructor(auth, postSvc) {
+    this.postSvc = postSvc;
     auth
       .getCurrentUser()
       .then(user => {
@@ -18,10 +22,10 @@ export class Controller {
   }
 
   $onInit() {
-    this.http
-      .get('/api/posts')
-      .then(response => {
-        this.posts = response.data.posts;
+    this.postSvc
+      .getList()
+      .then(data => {
+        this.posts = data.posts;
         this.posts.forEach(post => {
           const { PostLikes } = post;
           if(PostLikes) {
@@ -36,10 +40,9 @@ export class Controller {
 
   add(message) {
     if(message) {
-      this.http
-        .post('/api/posts', { post: { message }})
-        .then(response => {
-          const { data: { post }} = response;
+      this.postSvc
+        .create({ message })
+        .then(post => {
           post.User = this.currentUser;
           this.posts.unshift(post);
           this.newPostMessage = '';
@@ -51,10 +54,10 @@ export class Controller {
   }
 
   delete(post) {
-    this.http
-      .delete(`/api/posts/${post._id}`)
-      .then(response => {
-        if(response.data.post) {
+    this.postSvc
+      .delete(post._id)
+      .then(deleted => {
+        if(deleted) {
           const position = this.posts.indexOf(post);
           this.posts.splice(position, 1);
         }
@@ -63,10 +66,9 @@ export class Controller {
 
   reply(post, message) {
     if(message) {
-      this.http
-        .post('/api/posts', { post: { ParentId: post._id, message }})
-        .then(response => {
-          const { data: { post: newPost }} = response;
+      this.postSvc
+        .create({ ParentId: post._id, message })
+        .then(newPost => {
           newPost.User = this.currentUser;
           post.PostReplies = post.PostReplies || [];
           post.PostReplies.push(newPost);
@@ -77,29 +79,41 @@ export class Controller {
   }
 
   like(post) {
-    this.http
-      .post(`/api/posts/${post._id}/like`)
-      .then(response => {
-        const { data: { postLike }} = response;
-        post.PostLikes = post.PostLikes || [];
-        post.PostLikes.push(postLike);
+    this.postSvc
+      .like(post._id)
+      .then(postLike => {
         post.liked = !postLike.deletedAt;
+        post.PostLikes = post.PostLikes || [];
+
+        const foundLike = post.PostLikes.find(like => like._id === postLike._id);
+        if(!postLike.deletedAt && !foundLike) {
+          post.PostLikes.push(postLike);
+        } else if(postLike.deletedAt && foundLike) {
+          const position = post.PostLikes.indexOf(foundLike);
+          post.PostLikes.splice(position, 1);
+        }
       });
   }
 
 }
 
 Controller.$inject = [
-  '$http',
-  'Auth'
+  'Auth',
+  'PostService'
 ];
 
-export default angular.module('demoAppApp.main', [uiRouter])
-  .config(routing)
+export default angular
+  .module('demoAppApp.main', [uiRouter])
+  .config(Route)
   .component('main', {
     template: require('./main.html'),
     controller: Controller
   })
+  .component('notificationsButton', NotificationsButtonComponent)
+  .component('likeSummary', LikeSummaryComponent)
+  .service('UserService', UserService)
+  .service('NotificationService', NotificationService)
+  .service('PostService', PostService)
   .filter('reverse', function() {
     return function(items) {
       return items.slice().reverse();
